a6f7295fa4c8d8832e7f1217c6163162ad4c099c
koolbeza <bezawitbh7@gmail.com>
2023-05-12 03:46:38 -0700

    DONE

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,56 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-
+#define UNUSED(x)(void)(x)
 /**
- * _isnumber - checks if string is a number
- * @s: string
- *
- * Return: On success 1.
- * If not a number, 0 is returned.
+ * main - adds two numbers
+ * @argc:int
+ * @argv:array of strings
+ * Return: sum if all digits
  */
-
-int _isnumber(char *s)
+int main(int argc, char *argv[])
 {
-	int i, check, d;
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
 
-	i = 0, d = 0, check = 1;
-	if (*s == '-')
+	sum = 0;
+	c = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < '0' || s[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
 		i++;
-	for (; *(s + i) != 0; i++)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
 	}
-	return (check);
-}
-/**
- * main - Entry point
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
- */
-int main(int argc, char **argv)
-{
-	int i, n, ex;
 
-	ex = 0, n = 0;
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (_isnumber(argv[i]))
-				n += atoi(argv[i]);
-			else
-				ex = 1;
-		}
+		c = atoi(argv[i]);
+		sum = sum + c;
 	}
-	if (ex == 0)
-		printf("%i\n", n);
-	else
-		printf("%s\n", "Error");
-	return (ex);
+	printf("%d\n", sum);
+	return (0);
 }
